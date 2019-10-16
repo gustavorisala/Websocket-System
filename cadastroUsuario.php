a40be0dd31a5e7f09245595fcccd3ea65ef291ae
gutavo <gustavorisala@gmail.com>
2019-10-16 10:55:03 -0300

    no message

--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -2,9 +2,9 @@
 
 $i=0;
 include 'controle/cadastrarNovoUsuario.php';
-if (isset($_POST["a"])) {
-    $i = $_POST["a"];
-    echo $i;
+if (isset($_GET["a"])) {
+    $i = $_GET["a"];
+
 }
 ?>
 
@@ -16,10 +16,86 @@ if (isset($_POST["a"])) {
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<!-- jquery - link cdn -->
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+<!-- Bootstrap -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+
+<!-- bootstrap - link cdn -->
+
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
 <title>Pre Cadastro MT BRASIL</title>
 <link rel="icon" href="imagens/favicon.png">
 </head>
+<style>
+html, body
+{
+  height: 100%;
+}
+	body
+	{
+		  background: url("https://i.pinimg.com/originals/0a/63/0b/0a630be2b073f47d06a784ab085d46b1.jpg");
+}
+html, p
+{
+	font-family: "Gotham SSm A","Gotham SSm B","Helvetica Neue",sans-serif !important;
+}
 
+	.collogin
+	{
+	    box-shadow: 0 0 100px black;
+		background-color: black;
+		height: 100%;
+	}
+	.box-login
+	{
+    padding-top: 5% !important;
+			padding: 15%;
+}
+/*campos texto */
+label
+{
+	font-size: 18px;
+color: white;
+}
+/* botao enviar*/
+.btn
+{
+	border-color: white;
+	font-family: "Gotham SSm A","Gotham SSm B","Helvetica Neue",sans-serif !important;
+	font-size: 18px;
+	color: white;
+	background-color: #bb914a;
+}
+/* bloco erro*/
+.form-control
+{
+  border: 2px solid #bb914a7a
+}
+.errormsg
+{
+padding-top: 1%;
+}
+.rowtxtregister
+{
+	padding-top: 2%;
+}
+.txtregister
+{
+	font-family: "Gotham SSm A","Gotham SSm B","Helvetica Neue",sans-serif !important;
+	font-size: 14px;
+	color: white;
+}
+.btn:hover
+{
+  background-color: white;
+  color: black;
+}
+</style>
 <body>
 
 	<div id="navbar" class="navbar-collapse collapse">
@@ -28,7 +104,7 @@ if (isset($_POST["a"])) {
 		</ul>
 	</div>
 	<!--/.nav-collapse -->
-	     	
+
 <?php
 
 
@@ -40,48 +116,53 @@ if (isset($erro))
 ?>
 
 
-<div class="box-parent-login">
-		<div class="well bg-white box-login">
-			<h1 class="ls-login-logo">Pre Cadastro MT BRASIL</h1>
+<div class="container containerlogin h-100">
+  <div class="row rowlogin h-100">
+    <div class="col-6 mx-auto collogin">
+    <div class="well box-login">
+      <img class="img-fluid" src="images/logo.jpeg" />
 			<form role="form" action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
 				<fieldset>
 
 					<div class="form-group ls-login-user">
 						<label for="userLogin">Nome</label> <input
 							class="form-control ls-login-bg-user input-lg" id="nome"
-							type="text" aria-label="Nome" name="nome" placeholder="Nome">
+							type="text" aria-label="Nome" name="nome" placeholder="Insira seu nome">
 					</div>
 
 					<div class="form-group ls-login-user">
-						<label for="email">E-MAIL</label> <input
+						<label for="email">E-mail</label> <input
 							class="form-control ls-login-bg-password input-lg" id="email"
 							type="text" aria-label="E-mail" name="emailUser"
-							placeholder="email">
+							placeholder="Insira seu e-mail">
 					</div>
-
-					<div class="form-group ls-login-password">
+          <div class="row">
+					<div class="form-group col ls-login-password">
 						<label for="userPassword">Senha</label> <input
 							class="form-control ls-login-bg-password input-lg"
 							id="userPassword" type="password" aria-label="senha"
-							name="senhaUser" placeholder="senha">
+							name="senhaUser" placeholder="Insira sua senha">
 					</div>
 
-					<div class="form-group ls-login-password">
+					<div class="form-group col ls-login-password">
 						<label for="userPassword">Confirmar Senha</label> <input
 							class="form-control ls-login-bg-password input-lg"
 							id="userPassword2" type="password" aria-label="confirmar senha"
-							name="confirmarSenhaUser" placeholder="senha">
+							name="confirmarSenhaUser" placeholder="Repita sua senha">
 					</div>
+        </div>
 
 
 					<input type="hidden" value="<?php echo $i?>" name="indicacao"> <input
-						type="submit" value="Enviar"
+						type="submit" value="CONTINUAR"
 						class="btn btn-primary btn-lg btn-block">
 
 
 				</fieldset>
 			</form>
 		</div>
+  </div>
+</div>
 	</div>
 
 </body>
--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include 'controle/validar_acesso.php';
 
 <!-- Bootstrap -->
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="estilo.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
 
 <!-- bootstrap - link cdn -->
 
@@ -31,13 +31,72 @@ include 'controle/validar_acesso.php';
     <![endif]-->
 </head>
 
+<style>
+html,body
+{
+	height: 100%;
+}
+	body
+	{
+		  background: url("https://i.pinimg.com/originals/0a/63/0b/0a630be2b073f47d06a784ab085d46b1.jpg");
+}
+html, p
+{
+	font-family: "Gotham SSm A","Gotham SSm B","Helvetica Neue",sans-serif !important;
+}
+
+	.collogin
+	{
+	    box-shadow: 0 0 100px black;
+		background-color: black;
+		height: 100%;
+	}
+	.box-login
+	{
+			padding: 15%;
+}
+/*campos texto */
+label
+{
+	font-size: 18px;
+color: white;
+}
+/* botao enviar*/
+.btn
+{
+	border-color: white;
+	font-family: "Gotham SSm A","Gotham SSm B","Helvetica Neue",sans-serif !important;
+	font-size: 18px;
+	color: white;
+	background-color: #bb914a;
+}
+/* bloco erro*/
+.errormsg
+{
+padding-top: 1%;
+}
+.form-control
+{
+	border: 2px solid #bb914a7a;
+}
+.rowtxtregister
+{
+	padding-top: 2%;
+}
+.txtregister
+{
+	font-family: "Gotham SSm A","Gotham SSm B","Helvetica Neue",sans-serif !important;
+	font-size: 14px;
+	color: white;
+}
+</style>
 <body>
 
-
-	<div align="center">
-		<div class="box-parent-login" style="width: 80%">
-			<div class="well bg-white box-login">
-				<h1 class="ls-login-logo">MT BRASIL</h1>
+	<div class="container containerlogin h-100">
+		<div class="row rowlogin h-100">
+			<div class="col-6 mx-auto collogin">
+			<div class="well box-login">
+				<img class="img-fluid" src="images/logo.jpeg" />
 				<form role="form" action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
 					<fieldset>
 
@@ -45,33 +104,38 @@ include 'controle/validar_acesso.php';
 							<label for="userLogin">E-mail</label> <input
 								class="form-control ls-login-bg-user input-lg" id="userLogin"
 								type="text" aria-label="E-mail" name="usuario"
-								placeholder="E-mail">
+								placeholder="Insira seu e-mail">
 						</div>
 
 						<div class="form-group ls-login-password">
 							<label for="userPassword">Senha</label> <input
 								class="form-control ls-login-bg-password input-lg"
 								id="userPassword" type="password" aria-label="Senha"
-								name="senha" placeholder="Senha">
+								name="senha" placeholder="Insira sua senha">
 						</div>
 
-						<div>
-							<a href="resetarSenha.php">Resetar Senha</a><br><br>
-						</div>
 
-						<input type="submit" value="Entrar"
-							class="btn btn-primary btn-lg btn-block">
+
+						<input type="submit" value="ENTRAR"
+							class="btn btn-lg btn-block">
 
 
 					</fieldset>
 				</form>
-				
+				<div class="row rowtxtregister">
+				  <div class="col text-center">
+						<span class="txtregister">
+				    Ainda não sou membro<a href="cadastroUsuario.php">  CRIAR CONTA</a>
+					</span>
+				  </div>
+				</div>
 				<?php
     if (isset($erro))
-        echo '<div style="color:#F00">' . utf8_encode($erro) . '</div><br/><br/>';
+        echo '<div class="errormsg" style="color:#F00">' . utf8_encode($erro) . '</div><br/><br/>';
 
     ?>
 			</div>
+		</div>
 		</div>
 	</div>
 </body>
