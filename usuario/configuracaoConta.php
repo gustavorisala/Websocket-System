54bf4e4bc51fc96e22a8991a0dd286442b858d52
pubek <pubek@DESKTOP-MUC171I>
2019-10-23 17:54:07 -0300

    link de afliacao

--- usuario/configuracaoConta.php
+++ usuario/configuracaoConta.php
@@ -16,70 +16,81 @@ require_once '../controle/dadosConfigurarConta.php';
 <!-- bootstrap - link cdn -->
 <link rel="stylesheet"
 	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<link rel="stylesheet"
-		href="../css/style.css">
+<link rel="stylesheet" href="../css/style.css">
 
 </head>
 <body>
 
-	  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<button class="navbar-toggler" type="button" data-toggle="collapse"
+			data-target="#conteudoNavbarSuportado"
+			aria-controls="conteudoNavbarSuportado" aria-expanded="false"
+			aria-label="Alterna navegação">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-	  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-	    <ul class="navbar-nav mr-auto">
-	      <a class="navbar-brand" href="#">
-	  <img class="ml-4 logotipo" src="../images/logosemfundo.png" alt="">
-	</a>
-	    </ul>
+		<div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+			<ul class="navbar-nav mr-auto">
+				<a class="navbar-brand" href="#"> <img class="ml-4 logotipo"
+					src="../images/logosemfundo.png" alt="">
+				</a>
+			</ul>
 	    <?php
-	    if ($papel == "cliente") {
-	        include_once '../menus/cliente.php';
-	    }
+    if ($papel == "cliente") {
+        include_once '../menus/cliente.php';
+    }
 
-	    if ($papel == "consultor") {
-	        include_once '../menus/consultor.php';
-	    }
+    if ($papel == "consultor") {
+        include_once '../menus/consultor.php';
+    }
 
-	    if ($papel == "consultorM") {
-	        include_once '../menus/consultorMaster.php';
-	    }
+    if ($papel == "consultorM") {
+        include_once '../menus/consultorMaster.php';
+    }
 
-	    if ($papel == "admin") {
-	        include_once '../menus/admin.php';
-	    }
-	    ?>
+    if ($papel == "admin") {
+        include_once '../menus/admin.php';
+    }
+    ?>
 	  </div>
 	</nav>
 	<div class="container-fluid">
-	<div class="row headerdados">
-	  <div class="col text-center">
-	    <h1>Pagina do usuário</h1>
-	  </div>
-	</div>
+		<div class="row headerdados">
+			<div class="col text-center">
+				<h1>Pagina do usuário</h1>
+			</div>
+		</div>
 	</div>
 
 
 	<div class="container-fluid containerdados">
-	<div class="row">
-		<div id="menu" class="col-3 paineluser">
+		<div class="row">
+			<div id="menu" class="col-3 paineluser">
 
 
-			<div class="dadosusuario">
-				<ul class="sidebar"><h3>Dados pessoais</h3>
-				<li><img src="../images/gmail.svg"> <?=$dados_usuario['email']?></li>
-				<li><img src="../images/password.svg"> <?=$dados_usuario['nome']?></li>
-				<li><img src="../images/link.svg"> <a class="btn copiarlink" data-clipboard-target="#link">
-					<span>Copiar Link</span></a></li>
-					<input id="link" value="https://copytraderbrasil.com.br?a=<?=$dados_usuario['codigoindicacao']?>">
-			</ul>
+				<div class="dadosusuario">
+					<ul class="sidebar">
+						<h3>Dados pessoais</h3>
+						<li><img src="../images/gmail.svg"> <?=$dados_usuario['email']?></li>
+						<li><img src="../images/password.svg"> <?=$dados_usuario['nome']?></li>
+						<li><img src="../images/link.svg"> <a class="btn copiarlink"
+							data-clipboard-target="#link"> <span>Copiar Link</span></a></li>
+						<input id="link"
+							value="<?php
+    if ($papel == "consultor")
+        echo "https://app.copytraderbrasil.com.br/provedorSinaisKleyton?a=".$dados_usuario['codigoindicacao'];
+    if ($papel == "consultorM")
+        echo "https://app.copytraderbrasil.com.br/provedorSinaisKleyton";
+    if ($papel == "cliente")
+        echo "https://app.copytraderbrasil.com.br/cadastroUsuario.php?a=\"" . $dados_usuario['codigoindicacao'] . "\"";
+    ?>">
+					</ul>
+				</div>
+
 			</div>
 
-</div>
-
-<div class="col-9 colinfos formdados">
-<div class="container">
+			<div class="col-9 colinfos formdados">
+				<div class="container">
 			<?php
 
 if (isset($erro))
@@ -92,63 +103,65 @@ else if (isset($sucesso))
 			<br /> <br />';
 ?>
 			<div class="row">
-			<div class="col-md-6 mx-auto colcel">
-				<p class="alterartxt">Alterar Senha</p>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text" id="basic-addon1">Senha atual</span>
-					</div>
-					<form class="" action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
-					<input type="password" class="form-control"
-						placeholder="Insira sua senha" aria-label="Senha atual"
-						aria-describedby="basic-addon1" name="senhaatual">
-				</div>
+						<div class="col-md-6 mx-auto colcel">
+							<p class="alterartxt">Alterar Senha</p>
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="basic-addon1">Senha atual</span>
+								</div>
+								<form class="" action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
+									<input type="password" class="form-control"
+										placeholder="Insira sua senha" aria-label="Senha atual"
+										aria-describedby="basic-addon1" name="senhaatual">
+							
+							</div>
 
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text" id="basic-addon2">Nova senha</span>
-					</div>
-					<input type="password" class="form-control" minlength="5"
-						placeholder="Insira a nova senha" aria-label="Nova senha"
-						aria-describedby="basic-addon2" name="novasenha">
-				</div>
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="basic-addon2">Nova senha</span>
+								</div>
+								<input type="password" class="form-control" minlength="5"
+									placeholder="Insira a nova senha" aria-label="Nova senha"
+									aria-describedby="basic-addon2" name="novasenha">
+							</div>
 
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text" id="basic-addon3">Confirmar senha</span>
-					</div>
-					<input type="password" class="form-control" minlength="5"
-						placeholder="Repita a nova senha"
-						aria-label="Repita a nova senha" aria-describedby="basic-addon3"
-						name="confirmacaosenhanova">
-				</div>
-        <div class="col text-center p-0 pt-3">
-				<button class="btn" type="submit">Confirmar</button>
-      </div>
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text" id="basic-addon3">Confirmar
+										senha</span>
+								</div>
+								<input type="password" class="form-control" minlength="5"
+									placeholder="Repita a nova senha"
+									aria-label="Repita a nova senha"
+									aria-describedby="basic-addon3" name="confirmacaosenhanova">
+							</div>
+							<div class="col text-center p-0 pt-3">
+								<button class="btn" type="submit">Confirmar</button>
+							</div>
 
+						</div>
+					</div>
+				</div>
+				</form>
 			</div>
 		</div>
-</div>
-		</form>
 	</div>
-  	</div>
-	  	</div>
 
-      <!-- Footer -->
-      <footer class="page-footer font-small special-color-dark pt-4">
+	<!-- Footer -->
+	<footer class="page-footer font-small special-color-dark pt-4">
 
-        <!-- Footer Elements -->
-        <!-- Footer Elements -->
+		<!-- Footer Elements -->
+		<!-- Footer Elements -->
 
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2019 Copyright
-          <a href="#"> MTBrasil</a>
-        </div>
-        <!-- Copyright -->
+		<!-- Copyright -->
+		<div class="footer-copyright text-center py-3">
+			© 2019 Copyright <a href="#"> MTBrasil</a>
+		</div>
+		<!-- Copyright -->
 
-      </footer>
-      <!-- Footer -->
-			<script>
+	</footer>
+	<!-- Footer -->
+	<script>
 			new ClipboardJS('.btn');
 			</script>
 </body>
