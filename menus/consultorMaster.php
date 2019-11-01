12f2c031108437a7090ce6d84b7d2cd863f29328
pubek <pubek@DESKTOP-MUC171I>
2019-10-31 22:23:30 -0300

    ccommit

--- menus/consultorMaster.php
+++ menus/consultorMaster.php
@@ -5,8 +5,8 @@
 			<li><a class="navbar-brand" href="../admin/consultorMaster.php"><?php echo utf8_encode("Inicio");?></a></li>
 			<li><a class="navbar-brand" href="../admin/inserirConsultor.php"><?php echo utf8_encode("Inserir consultor");?></a></li>
 			<li><a class="navbar-brand" href="../admin/pagamentos.php"><?php echo utf8_encode("Pagamentos");?></a></li>
-			<li><a class="navbar-brand" href="../admin/operacoesAll.php"><?php echo utf8_encode("Todas as operaÃ§Ãµes");?></a></li>
-			<li><a class="navbar-brand" href="../usuario/configuracaoConta.php"><?php echo utf8_encode("ConfiguraÃ§Ãµes");?></a></li>
+			<li><a class="navbar-brand" href="../admin/operacoesAll.php"><?php echo utf8_encode("Todas as operações");?></a></li>
+			<li><a class="navbar-brand" href="../usuario/configuracaoConta.php"><?php echo utf8_encode("Configurações");?></a></li>
 			<li><a href="../sair.php">Sair</a></li>
 		</ul>
 
