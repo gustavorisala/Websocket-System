85554e006843b0a0cc77179d099a33127a4351e4
pubek <pubek@DESKTOP-MUC171I>
2019-10-31 21:58:52 -0300

    ssss

--- menus/cliente.php
+++ menus/cliente.php
@@ -6,7 +6,7 @@
 			<li><a class="navbar-brand" href="../cliente/"><?php echo utf8_encode("Inicio");?></a></li>
 
 			<li><a class="navbar-brand" href="../cliente/operacoes.php"><?php echo utf8_encode("Entradas");?></a></li>
-
+ 
 			<li><a class="navbar-brand" href="../usuario/configuracaoConta.php"><?php echo utf8_encode("Configurações");?></a></li>
 
 			<li><a href="../sair.php">Sair</a></li>
