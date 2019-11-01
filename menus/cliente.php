9435a25b50b3297e9dafa18466f8c267f29dba89
pubek <pubek@DESKTOP-MUC171I>
2019-11-01 10:21:57 -0300

    horarios 2

--- menus/cliente.php
+++ menus/cliente.php
@@ -4,11 +4,9 @@
 	<div class="" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 			<li><a class="navbar-brand" href="../cliente/"><?php echo utf8_encode("Inicio");?></a></li>
-<li><a class="navbar-brand" href="../cliente/horarios.php"><?php echo utf8_encode("Horarios");?></a></li>
+			<li><a class="navbar-brand" href="../cliente/horarios.php"><?php echo utf8_encode("Horarios");?></a></li>
 			<li><a class="navbar-brand" href="../cliente/operacoes.php"><?php echo utf8_encode("Entradas");?></a></li>
- 
 			<li><a class="navbar-brand" href="../usuario/configuracaoConta.php"><?php echo utf8_encode("Configurações");?></a></li>
-
 			<li><a href="../sair.php">Sair</a></li>
 		</ul>
 
