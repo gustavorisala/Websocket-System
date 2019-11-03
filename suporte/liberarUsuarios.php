f2e8747910bcb40eaa9223522cdd17446bc45ac7
gutavo <gustavorisala@gmail.com>
2019-11-03 15:25:29 -0300

    no message

--- suporte/liberarUsuarios.php
+++ suporte/liberarUsuarios.php
@@ -18,6 +18,9 @@ if (! isset($_SESSION['usuario'])) {
 <link rel="icon" href="../imagens/favicon.png">
 <!-- jquery - link cdn -->
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="stylesheet" href="http://www.copytraderbrasil.com.br/css/backoffice/suporte.css">
 <link rel="stylesheet"
 	href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 <link rel="stylesheet"
@@ -35,24 +38,172 @@ if (! isset($_SESSION['usuario'])) {
 </head>
 
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
+    <style>
+    .formdados
+    {
+    	border: 0px;
+    }
+    body
+    {
+    	background: unset !important;
+    }
+    #conteudo
+    {
+      background-size: cover;
+      background-image: url(../images/2.jpeg);
+    }
+    .sidebar li
+    {
+        position: relative;
+    	font-size: 16px;
+    }
+    .sidebar img
+    {
+    	width: 15%;
+    	position: relative;
+    }
+    .centralizar
+    {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  #createiq
+  {
+      background-size: cover;
+      background-image: url(../images/3.jpeg);
+    }
+    footer
+    {
+      position: relative;
+      bottom: 0;
+      left: 0;
+    }
 
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-				
-			</ul>
-			<ul class="navbar-nav  my-2 my-lg-0">
-				<li><a href="../sair.php">Sair</a></li>
-			</ul>
+  .bootstrap-select > .dropdown-toggle
+  {
+    font-weight: bold;
+    padding: 4%;
+    height: 3.3rem;
+  }
+  .input-group-prepend {
+      margin-right: -1px;
+      z-index: 9999999;
+  }
+  .input-group-text
+  {
+    font-weight: bold;
+  }
+  .createtitulo
+  {
+    display: flex;
+      justify-content: center;
+      align-items: center;
+    background-color: #e9ecef;
+    padding: 3%;
+    border-color: transparent !important;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+  }
+  #blococriar
+  {
+    background-color: white;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    padding-left: 5rem;
+    padding-right: 5rem;
+    border: 1rem solid #e9ecef;
+  }
+  #blocolembrete ol h1
+  {
+    background-color: black;
+    color:white;
+    text-align: center;
+  }
+  #blocolembrete li
+  {
+    padding: 2%;
+    background-color: #49505724;
+  }
+  .padding
+  {
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+  }
+  .btn
+  {
+    font-size: 16px;
+    font-weight: 500;
+  padding: 2%;
+  border-radius: 5px;
+  border-color: white;
+  font-family: "Gotham SSm A","Gotham SSm B","Helvetica Neue",sans-serif !important;
+  color: black;
+  background-color: #e9ecef;
+  }
+  .imguteis img
+  {
+    width: 10%;
+    margin: 3%;
+  }
+  @media (max-width:767px) {
+    #blococriar
+    {
+      padding: 1rem;
+    }
+    .btn
+    {
+      width: 15rem;
+      padding: 1rem;
+    }
+    #blocolembrete ol
+    {
+      padding-left: 0px;
+    }
+    .bootstrap-select
+    {
+      margin-bottom: 1rem;
+    }
+    .input-group-prepend
+    {
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+  </style>
+  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  		<button id="navtoggle" class="navbar-toggler" type="button" data-toggle="collapse"
+  			data-target="#conteudoNavbarSuportado"
+  			aria-controls="conteudoNavbarSuportado" aria-expanded="false"
+  			aria-label="Alterna navegação">
+  			<span class="navbar-toggler-icon"></span>
+  		</button>
 
-			<!--/.nav-collapse -->
-
+  		<div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+  			<ul class="navbar-nav mr-auto">
+  				<a class="navbar-brand" href="#"> <img class="ml-4 logotipo"
+  					src="../images/logosemfundo.png" alt="">
+          </div>
+  				</a>
+          <a href="../sair.php">Sair</a>
+  			</ul>
+  	  </div>
+  	</nav>
+  	<div class="container-fluid">
+  		<div class="row headerdados">
+  			<div class="col text-center">
+  				<h1>Painel Suporte</h1>
+  			</div>
+  		</div>
 		</div>
-	</nav>
-
-	<div align="center">
-
+    <div id="conteudo" class="container-fluid">
+  		<div class="row">
+  			<div class="col">
+	<div class="container-fluid">
+<div class="row padding">
+  <div class="col-12 col-sm-6 mx-auto">
+    <div class="col col-sm-11 mx-auto">
 <?php
 
         if (isset($erro))
@@ -61,78 +212,114 @@ if (! isset($_SESSION['usuario'])) {
             echo '<div style="color:#00f">' . utf8_encode($sucesso) . '</div><br/><br/>';
 
         ?>
-
-
 		<form action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
-			<p></p>
-			<div style="width: 80%">
-
-
-				<div class="input-group mb-3">
+				<div class="input-group">
 					<div class="input-group-prepend">
-						<span class="input-group-text" id="basic-addon3">Contas Pre
-							Cadastro</span>
+						<span class="input-group-text" id="basic-addon3">Contas Pré Cadastro</span>
 					</div>
-
-					<select class="selectpicker" data-live-search="true" name="email">
+					<select class="selectpicker pl-0 col-12 col-sm-6 pr-0" data-live-search="true" name="email">
 
 <?php
         if ($dadosUsuariosPre) {
-
             while ($row = mysqli_fetch_array($dadosUsuariosPre)) {
-
                 echo "<option value=\"" . $row['email'] . "\">" . $row['email'] . "</option>";
             }
         }
-
         ?>
-      
-
-
-					</select> <input type='submit' value='Selecionar'>
+      </select>
+      <input class="ml-4 btn" type='submit' value='Selecionar'>
 				</div>
-
-				<div>
-				
+    </div>
+  </div>
+</div>
+  </div>
+				<div class="row padding">
+          <div id="blocolembrete" class="col-12 col-sm-4 centralizar">
+                <ol><h1>Lembrar-se</h1>
+            		<li>Cadastrar usuário na IQ Option</li>
+              		<li>Confirmar pagamento da licença de 90 dias</li>
+            		<li>Liberar o usuario no backoffice</li>
+            		<li>Enviar link do programa MT BRASIL</li>
+            		<li>Enviar informações por e-mail</li>
+              </ol>
+              </div>
+      				<div id="createiq" class="col-12 col-sm-4">
 				<?php if(isset($dadosUser)){?>
-				<h2>Criar Conta na IQ</h2>
+          <div class="row createtitulo">
+				<h1>Criar Conta na IQ Option</h1>
+      </div>
+      <div id="blococriar" class="row">
+        <div class="col">
 					<h4>Nome: <?=$dadosUser["nome"]?></h4>
 					<h4>Email: <?=$dadosUser["email"]?></h4>
 					<h4>Senha: <?=$senhaTemp?></h4>
-
-					<input type="checkbox" value="contaIQ" name="contaIQ"> Confirmar Conta IQ Criada<br>
-					<input type="checkbox" value="pagamento" name="pagamento"> Confirmar Pagamento
-					Assinatura
-				</div>
-
-
-
+          <div class="row mt-3 mb-3">
+            <div class="col mx-auto">
+              <div class="row">
+                <div class="col">
+					<input type="checkbox" value="contaIQ" name="contaIQ"> Confirmar Conta IQ Criada
+        </div>
+        </div>
+        <div class="row">
+            <div class="col">
+					<input type="checkbox" value="pagamento" name="pagamento"> Confirmar Pagamento Assinatura
+        </div>
+        </div>
+        </div>
+      </div>
+        <div class="row">
+          <div class="col">
 				<input type="hidden" name="adm" value="<?=$_SESSION['id_usuario']?>">
-				<input type='submit' value='Liberar Usuario'>
-			</div>
+				<input type='submit' class="btn p-3" value='Liberar Usuário'>
+      </div>
+    </div>
+      </div>
+      </div>
+      </div>
+      <div class="col">
+        <div class="row">
+          <div class="col text-center">
+            <h1>Links Úteis</h1>
+          </div>
+        </div>
+          <div class="row imguteis">
+            <div class="col text-center">
+              <a rel="noopener noreferrer" target="_blank" href="https://webmail-seguro.com.br/copytraderbrasil.com.br/"><img class="img-fluid" src="../images/email.svg"></a>
+                <a rel="noopener noreferrer" target="_blank" href="https://dashboard.tawk.to/login?lang=pt_br"><img class="img-fluid" src="../images/callcenter.svg"></a>
+              </div>
+          </div>
+      </div>
+            </div>
 
-
-<?php
-        }
-        ?>
-
-
-		</form>
-
-
-
-		<br><br><br>
-		<p>Cadastro na IQ option ok</p>
-		<p>Confirmacao de pagamento da Licenca 90 dias</p>
-		<p>liberacao do usuario no backoffice</p>
-		<p>link do programa MT BRASIL</p>
-		<p>liberacao do usuario no backoffice</p>
-		<p>envio das informacoes por email</p>
-
-
-
-
+          <?php }  ?>
+        </form>
 	</div>
+</div>
+</div>
+
+	<!-- Footer -->
+	<footer class="page-footer font-small special-color-dark pt-4">
+
+		<!-- Footer Elements -->
+		<!-- Footer Elements -->
+
+		<!-- Copyright -->
+		<div class="footer-copyright text-center py-3">
+			© 2019 Copyright <a href="#"> MTBrasil</a>
+		</div>
+		<!-- Copyright -->
+
+	</footer>
+	<!-- Footer -->
+	<script>
+			new ClipboardJS('.btn');
+			</script>
+
+		<script>
+		$("#navtoggle").click(function(){
+		  $("#conteudoNavbarSuportado").toggle();
+		});
+		</script>
 
 </body>
 </html>
